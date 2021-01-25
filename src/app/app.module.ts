1f02d3bf29119b906828496266328019783fc1b8
Gregor Laan <laangregor@gmail.com>
2021-01-25 20:38:40 +0200

    Generate a new component: row

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RowComponent } from './row/row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RowComponent
   ],
   imports: [
     BrowserModule
