244d72a626186a3403bab906af9bb2704be58f04
Gregor Laan <laangregor@gmail.com>
2021-01-30 16:45:01 +0200

    Stock row basic design

--- src/app/row/row.component.ts
+++ src/app/row/row.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Stock } from '../stock';
 
 @Component({
   selector: 'app-row',
@@ -6,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./row.component.css']
 })
 export class RowComponent implements OnInit {
+  stocks: Stock[] = [
+    {
+      "symbol": "AAPL",
+      "name": "Apple",
+      "price": 1000
+    },
+    {
+      "symbol": "MSFT",
+      "name": "Microsoft",
+      "price": 250
+    },
+    {
+      "symbol": "WFC",
+      "name": "Wells Fargo & Company Common Stock",
+      "price": 28
+    },
+    {
+      "symbol": "UBER",
+      "name": "Uber",
+      "price": 41
+    },
+    {
+      "symbol": "LYFT",
+      "name": "Lyft",
+      "price": 57
+    }
+  ];
 
   constructor() { }
 
